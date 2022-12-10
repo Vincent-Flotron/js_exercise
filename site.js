8659dfbfa9c6dc7aa00bbb70e7bed86ee73bee37
Vincent-Flotron <vincent.flotron@outlook.com>
2022-12-11 00:41:07 +0100

    site.js linked

---
+++ site.js
@@ -0,0 +1,7 @@
+function greet() {
+    console.log('Hey there clicker!');
+}
+
+function add_a_line(line) {
+    document.getElementById("logs").innerHTML += "<div class=\"log-line\"><p>a line</p></div>"
+}
